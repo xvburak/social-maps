f52f97cbd998788e0cd612088495612ffb6323fa
xvburak <xvburak@vutbr.cz>
2022-10-26 22:48:42 +0200

    setup

---
+++ src/routes/+error.svelte
@@ -0,0 +1,3 @@
+
+
+<p>some error :(</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-netlify';
 
+export default {
+  kit: {
+    // default options are shown
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: [
-		preprocess({
-		  postcss: true,
-		}),
-	  ],
-	kit: {
-
-		adapter: adapter()
-		
-	}
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    })
+  }
 };
-
-export default config;
