a8791375f3f6868da6c1059f0c9c2ca802388f7f
xvburak <xvburak@vutbr.cz>
2022-11-07 13:02:57 +0100

    75vh

--- src/lib/components/scrollspy/ScrollSection.svelte
+++ src/lib/components/scrollspy/ScrollSection.svelte
@@ -39,7 +39,7 @@
           flex-direction: column;
           align-items: center;
           justify-content: center;
-          min-height: 100vh;
+          min-height: 75vh;
           transition: .5s ease-in-out;
     }
       section.active {
