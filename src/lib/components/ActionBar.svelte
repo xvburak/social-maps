2a98cbce43e277daa328db3aaadccd30c5d7f228
xvburak <xvburak@vutbr.cz>
2022-11-07 12:46:00 +0100

    test

--- src/lib/components/ActionBar.svelte
+++ src/lib/components/ActionBar.svelte
@@ -3,7 +3,7 @@
     export let copy
 </script>
 
-<div class="h-12 bg-amber-300 fixed flex flex-col justify-center  bottom-0 w-full">
+<div class="h-12 bg-amber-300 fixed flex flex-col justify-center z-[90]  bottom-0 w-full">
     <div class="w-full">
         <a class="text-center block" href="{link}">{copy}</a>
     </div> 
